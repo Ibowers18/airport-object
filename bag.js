efe851b8b1328429d0e103842c4f1660816ba64a
Bowers <v929702@ad.vzwcorp.com>
2021-11-19 17:29:54 -0600

    added gitignore

---
+++ bag.js
@@ -0,0 +1,12 @@
+class Bag{
+
+    constructor (weight){
+        this.weight=weight
+        this.weight= function () {
+            if (this.weight > 0 && this.weight
+                return true)
+        }
+    }
+}
+
+module.exports = Bag
